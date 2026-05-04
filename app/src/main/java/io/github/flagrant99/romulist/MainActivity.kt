a800c4cfe389dbbc59d1c3feb9cc601aa2a429b0
flagrant99 <gregorygoeppel@comcast.net>
2026-05-04 08:54:51 -0400

    Hide android nav bar by default

--- app/src/main/java/io/github/flagrant99/romulist/MainActivity.kt
+++ app/src/main/java/io/github/flagrant99/romulist/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import io.github.flagrant99.romulist.ui.theme.RomulistTheme
 
 class MainActivity : ComponentActivity()
@@ -12,6 +15,11 @@ class MainActivity : ComponentActivity()
     {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        val windowInsetsController = WindowCompat.getInsetsController(window, window.decorView)
+        windowInsetsController.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        windowInsetsController.hide(WindowInsetsCompat.Type.navigationBars())
+
         setContent {
             RomulistTheme {
                 RomulistApp()
