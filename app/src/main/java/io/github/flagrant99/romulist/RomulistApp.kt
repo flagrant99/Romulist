aec05dded41b306a2b97fefaa693b0cdeb151a2a
flagrant99 <gregorygoeppel@comcast.net>
2026-05-08 18:47:41 -0400

    getsures

--- app/src/main/java/io/github/flagrant99/romulist/RomulistApp.kt
+++ app/src/main/java/io/github/flagrant99/romulist/RomulistApp.kt
@@ -5,8 +5,8 @@ import android.content.Context
 import android.os.storage.StorageManager
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.gestures.waitForUpOrCancellation
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -33,6 +33,9 @@ import androidx.compose.ui.input.key.onKeyEvent
 import androidx.compose.ui.input.key.type
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.platform.LocalHapticFeedback
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.foundation.gestures.awaitEachGesture
+import androidx.compose.foundation.gestures.awaitFirstDown
 import android.view.KeyEvent
 import io.github.flagrant99.romulist.ui.theme.RomulistTheme
 import java.io.File
@@ -109,10 +112,13 @@ fun RomulistApp()
             if (favoriteFolder == null) {
                 currentScreen = AppDestinations.SET_HOME
             } else {
-                selectedFolder = favoriteFolder?.let { File(it) }
+                selectedFolder = File(favoriteFolder!!)
+                selectedFile = null
                 currentScreen = AppDestinations.HOME
             }
         }
+
+        var isHomeLongClick by remember { mutableStateOf(false) }
 
         Scaffold(
             modifier = Modifier
@@ -158,23 +164,34 @@ fun RomulistApp()
                                 disabledTextColor = Color.Gray
                             ),
                             modifier = if (destination == AppDestinations.HOME) {
-                                Modifier.combinedClickable(
-                                    interactionSource = interactionSource,
-                                    indication = null,
-                                    onClick = { handleHome() },
-                                    onLongClick = {
-                                        haptics.performHapticFeedback(HapticFeedbackType.LongPress)
-                                        currentScreen = AppDestinations.SET_HOME
+                                Modifier.pointerInput(Unit) {
+                                    awaitEachGesture {
+                                        awaitFirstDown(requireUnconsumed = false)
+                                        
+                                        // Wait for timeout or release
+                                        val up = withTimeoutOrNull(viewConfiguration.longPressTimeoutMillis) {
+                                            waitForUpOrCancellation()
+                                        }
+
+                                        if (up == null) {
+                                            // Long press!
+                                            isHomeLongClick = true
+                                            haptics.performHapticFeedback(HapticFeedbackType.LongPress)
+                                            currentScreen = AppDestinations.SET_HOME
+                                        }
                                     }
-                                )
+                                }
                             } else Modifier,
                             onClick = {
-                                if (destination == AppDestinations.BACK) {
-                                    handleBack()
-                                } else if (destination == AppDestinations.HOME) {
-                                    // Handled by modifier
-                                } else {
-                                    currentScreen = destination
+                                when (destination) {
+                                    AppDestinations.BACK -> handleBack()
+                                    AppDestinations.HOME -> {
+                                        if (!isHomeLongClick) {
+                                            handleHome()
+                                        }
+                                        isHomeLongClick = false
+                                    }
+                                    else -> currentScreen = destination
                                 }
                             },
                             label = { Text(destination.label) },
